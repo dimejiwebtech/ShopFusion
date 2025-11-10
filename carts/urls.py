aa6f4fcfc18bc669d3f06e0bb695b45e33b9d0f6
dimejiwebtech <dimejiweb@gmail.com>
2025-11-10 21:47:12 +0100

    Improved cart functionality

--- carts/urls.py
+++ carts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart, name='cart')
+    path('', views.cart, name='cart'),
+    path('add-to-cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('remove-from-cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
+    path('delete-from-cart/<int:product_id>/', views.delete_cart_item, name='delete_cart_item'),
 ]
