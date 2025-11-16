83ae64c196e045cf3fec92bdc9a727aa64079099
dimejiwebtech <dimejiweb@gmail.com>
2025-11-16 07:26:39 +0100

    Implemented Gmail SMTP

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from tinymce.models import HTMLField
@@ -88,3 +89,12 @@ class Variation(models.Model):
 
     def __str__(self):
         return f"{self.product.product_name} - {self.variation_value}"
+    
+
+class GmailToken(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    token_expiry = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
