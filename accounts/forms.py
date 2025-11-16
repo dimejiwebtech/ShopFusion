f43ba65e70aa080fdaa6227aec4d7cce28076e95
dimejiwebtech <dimejiweb@gmail.com>
2025-11-16 06:26:04 +0100

    auth setup

---
+++ accounts/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from .models import Account
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter password',
+        'class': 'w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-600 focus:ring-2 focus:ring-blue-100 transition'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm password',
+        'class': 'w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-600 focus:ring-2 focus:ring-blue-100 transition'
+    }))
+    
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'First name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Last name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'e.g. +1234567890'
+        self.fields['email'].widget.attrs['placeholder'] = 'example@email.com'
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-600 focus:ring-2 focus:ring-blue-100 transition'
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Account.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists. Please use a different email.")
+        return email
+
+
+    def clean(self):
+        cleaned_data = super(RegisterForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password:
+            if password != confirm_password:
+                raise forms.ValidationError("Passwords do not match!")
+            if len(password) < 8:
+                raise forms.ValidationError("Password must be at least 8 characters long!")
+        
+        return cleaned_data
