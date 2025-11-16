f18f25a659550b9727a69c8aea60b4d942ea9b3f
dimejiwebtech <dimejiweb@gmail.com>
2025-11-16 19:35:12 +0100

    improved authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,13 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('password-reset-validate/<uidb64>/<token>/', views.password_reset_validate, name='password_reset_validate'),
+
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('reset-password/', views.reset_password, name='reset_password'),
 ]
 
